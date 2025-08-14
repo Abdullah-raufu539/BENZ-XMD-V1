176a1d5cbec57ea55000a8c62ba8aaabff5956fd
Abdullah-raufu539 <abdullahraufu30@gmail.com>
2025-08-14 01:45:36 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'B͎E͎N͎Z͎I͎ C͎O͎M͎P͎A͎N͎Y͎ X͎M͎D͎',
+  packname: '★~ĂB Đ●ĽÅH ♡💲♧',
   author: '‎',
-  botName: "B͎E͎N͎Z͎ C͎O͎M͎P͎A͎N͎Y͎ X͎M͎D͎",
-  botOwner: '👑ALPHA-KING👑', // Your name
-  ownerNumber: '263718728504', //Set your number here without + symbol, just add country code & number without any space
+  botName: "★~ĂB Đ●ĽÅH ♡💲♧",
+  botOwner: '★~ĂB Đ●ĽÅH ♡💲♧', // Your name
+  ownerNumber: '2349014049114', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.8",
 };
